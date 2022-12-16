c5ed5c1530be45eaa9b0da522abe8defc5be9bbc
Tech-sisi <oluwagbengamoyinoluwa41763@gmail.com>
2022-12-15 20:18:42 -0800

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper - function to check upper class alphabets
+ * @c: integer param
+ * Return: something
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
