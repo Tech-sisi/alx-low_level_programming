1d60be20fa07bd1e4fd3de5ffcd8e4effa5dceca
Tech-sisi <oluwagbengamoyinoluwa41763@gmail.com>
2022-12-15 20:50:52 -0800

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - function to print numbers
+ * Return: something
+ */
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+		_putchar(x + '0');
+	_putchar('\n');
+}
