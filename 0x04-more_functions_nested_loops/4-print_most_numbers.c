8380e7d7e4545f8947ff765895c83246b733f1da
Tech-sisi <oluwagbengamoyinoluwa41763@gmail.com>
2022-12-15 20:59:53 -0800

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print most numbers
+ * Return: something
+ */
+print_most_numbers(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		if (x != 2 && != 4)
+			_putchar(x + '0');
+	}
+	_putchar('\n');
+}
